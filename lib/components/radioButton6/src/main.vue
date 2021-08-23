e05529f54aa1eec1efab2f5e77af607174f9f63d
yekeqi <yekq@getui.com>
2021-08-23 15:01:50 +0800

    feat: add radioButton6

---
+++ lib/components/radioButton6/src/main.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="yh-radio-button-6" :ref="ref">
+        <div class="item" v-for="(item,index) in options"
+            @click="itemClick(item,index)"
+            :key="item.label" :class="{'item-active':index === activeIndex}">{{item.label}}</div>
+    </div>
+</template>
+<script>
+export default {
+  name: 'YhRadioButton6',
+  props: {
+    activeIndex: Number,
+    options: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data () {
+    return {
+      ref: 'yh-radio-button-6'
+    }
+  },
+  methods: {
+    itemClick (item, index) {
+      this.$emit('itemClick', item, index)
+    }
+  }
+}
+</script>
