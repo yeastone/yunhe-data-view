9a0aaf6da2ed0cd99cb349421e74c48bc5da5751
yekeqi <yekq@getui.com>
2021-09-01 20:08:27 +0800

    update: time box update

--- lib/components/timeBox1/src/main.vue
+++ lib/components/timeBox1/src/main.vue
@@ -1,51 +1,51 @@
 <template>
-    <div class="yh-time-box-1" :ref="ref">
-        <div class="date" :style="{color: color}">{{date}}</div>
-        <div class="time" :style="{color: color}">{{time}}</div>
-    </div>
+  <div class="yh-time-box-1" :ref="ref">
+    <div class="date" :style="{ color: color }">{{ date }}</div>
+    <div class="time" :style="{ color: color }">{{ time }}</div>
+  </div>
 </template>
 <script>
 export default {
-  name: 'YhTimeBox1',
+  name: "YhTimeBox1",
   props: {
     color: {
       type: String,
-      default: '#00DEFE'
+      default: "#00DEFE"
     }
   },
-  data () {
+  data() {
     return {
-      ref: 'time-box-1',
-      date: '',
-      time: ''
-    }
+      ref: "time-box-1",
+      date: "",
+      time: ""
+    };
   },
-  created () {
-    this.updateTime()
+  created() {
+    this.updateTime();
     setInterval(() => {
-      this.updateTime()
-    }, 10)
+      this.updateTime();
+    }, 10);
   },
   methods: {
-    updateTime () {
-      const checkFormat = (str) => {
-        str = str.toString()
+    updateTime() {
+      const checkFormat = str => {
+        str = str.toString();
         if (str.length < 2) {
-          str = '0' + str
+          str = "0" + str;
         }
-        return str
-      }
-      const now = new Date()
-      const year = now.getFullYear()
-      const mon = checkFormat(now.getMonth() + 1)
-      const day = checkFormat(now.getDate())
-      const hour = checkFormat(now.getHours())
-      const min = checkFormat(now.getMinutes())
-      const sec = checkFormat(now.getSeconds())
+        return str;
+      };
+      const now = new Date();
+      const year = now.getFullYear();
+      const mon = checkFormat(now.getMonth() + 1);
+      const day = checkFormat(now.getDate());
+      const hour = checkFormat(now.getHours());
+      const min = checkFormat(now.getMinutes());
+      const sec = checkFormat(now.getSeconds());
 
-      this.date = `${year}年${mon}月${day}日`
-      this.time = `${hour}:${min}:${sec}`
+      this.date = `${year}年${mon}月${day}日`;
+      this.time = `${hour}:${min}:${sec}`;
     }
   }
-}
+};
 </script>
--- src/components/timeBox1/src/main.vue
+++ src/components/timeBox1/src/main.vue
@@ -1,68 +1,73 @@
 <template>
-    <div class="yh-time-box-1" :ref="ref">
-        <div class="date" :style="{color: color}">{{date}}</div>
-        <div class="time" :style="{color: color}">{{time}}</div>
-    </div>
+  <div class="yh-time-box-1" :ref="ref">
+    <div class="date" :style="{ color: color }">{{ date }}</div>
+    <div class="time" :style="{ color: color }">{{ time }}</div>
+  </div>
 </template>
 <script>
 export default {
-  name: 'YhTimeBox1',
+  name: "YhTimeBox1",
   props: {
     color: {
       type: String,
-      default: '#00DEFE'
+      default: "#00DEFE"
     }
   },
-  data () {
+  data() {
     return {
-      ref: 'time-box-1',
-      date: '',
-      time: ''
-    }
+      ref: "time-box-1",
+      date: "",
+      time: ""
+    };
   },
-  created () {
-    this.updateTime()
+  created() {
+    this.updateTime();
     setInterval(() => {
-      this.updateTime()
-    }, 10)
+      this.updateTime();
+    }, 10);
   },
   methods: {
-    updateTime () {
-      const checkFormat = (str) => {
-        str = str.toString()
+    updateTime() {
+      const checkFormat = str => {
+        str = str.toString();
         if (str.length < 2) {
-          str = '0' + str
+          str = "0" + str;
         }
-        return str
-      }
-      const now = new Date()
-      const year = now.getFullYear()
-      const mon = checkFormat(now.getMonth() + 1)
-      const day = checkFormat(now.getDate())
-      const hour = checkFormat(now.getHours())
-      const min = checkFormat(now.getMinutes())
-      const sec = checkFormat(now.getSeconds())
+        return str;
+      };
+      const now = new Date();
+      const year = now.getFullYear();
+      const mon = checkFormat(now.getMonth() + 1);
+      const day = checkFormat(now.getDate());
+      const hour = checkFormat(now.getHours());
+      const min = checkFormat(now.getMinutes());
+      const sec = checkFormat(now.getSeconds());
 
-      this.date = `${year}年${mon}月${day}日`
-      this.time = `${hour}:${min}:${sec}`
+      this.date = `${year}年${mon}月${day}日`;
+      this.time = `${hour}:${min}:${sec}`;
     }
   }
-}
+};
 </script>
 <style lang="less">
-.yh-time-box-1 {
-    min-width: 150px;
-    display: inline-block;
-    .date {
-        font-size: 18px;
-        letter-spacing: 0.6px;
-        line-height: 18px;
-        text-align: right;
-    }
-    .time {
-        font-size: 50px;
-        line-height: 50px;
-    }
+@font-face {
+  font-family: "fxLed";
+  src: url("../assets/FX-LED.TTF") format("truetype");
 }
-
+.yh-time-box-1 {
+  width: 175px;
+  display: inline-block;
+  .date {
+    font-size: 18px;
+    line-height: 18px;
+    text-align: right;
+    letter-spacing: 2px;
+  }
+  .time {
+    font-family: "fxLed";
+    font-size: 50px;
+    line-height: 50px;
+    letter-spacing: 0;
+  }
+}
 </style>
